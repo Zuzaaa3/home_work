12cc45c804b527066acbd626d8eb9d821ededc6b
Zuzaaa3 <ziadamravatar2412001@gmail.com>
2025-08-17 18:47:04 +0300

    ..

---
+++ Session2_HW/Question10.dart
@@ -0,0 +1,3 @@
+//Explain the purpose of the main function in Dart.
+
+//The main function is the entry point of every Dart application. It is where the program starts executing.
---
+++ Session2_HW/Question11.dart
@@ -0,0 +1,7 @@
+//Describe the difference between var, dynamic, and explicitly typed variables in Dart.
+
+// 1. var: The var keyword is used to declare a variable without specifying its type. Once assigned, the variable's type cannot change.
+
+// 2. dynamic: The dynamic data type allows you to declare a variable that can hold values of any type. Unlike var, a dynamic variable's type can change at runtime.
+
+// 3. Explicitly typed variables: You can explicitly specify the type of a variable when declaring it. like int, string and bool
