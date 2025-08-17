679e19e6304112345b2da0c6a53329d2879897b8
Zuzaaa3 <ziadamravatar2412001@gmail.com>
2025-08-17 18:14:20 +0300

    ..

---
+++ Session2_HW/Question01.dart
@@ -0,0 +1,4 @@
+void main() {
+  print("my name is Ziad");
+  print("my age is 24");
+}
---
+++ Session2_HW/Question02.dart
@@ -0,0 +1,9 @@
+void main() {
+  int birth_year = 2001;
+  String city = "Cairo";
+  bool like = true;
+
+  print("My birth year is " + birth_year.toString());
+  print("My city is " + city);
+  print("Do I like programming? " + like.toString());
+}
