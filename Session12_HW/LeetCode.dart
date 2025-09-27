cafd21664b6568cb223e8a65d70b85b2c5f7b5ae
Zuzaaa3 <ziadamravatar2412001@gmail.com>
2025-09-27 23:17:39 +0300

    ..

---
+++ Session12_HW/LeetCode.dart
@@ -0,0 +1,20 @@
+class Solution {
+  ListNode? reverseList(ListNode? head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode? newHead = reverseList(head.next);
+
+    head.next!.next = head;
+    head.next = null;
+
+    return newHead;
+  }
+}
+
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
